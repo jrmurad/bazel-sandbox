be9039cf582350cd20b7b46c5b1e14ac6f3ceaf0
Jason Murad <jrmurad@gmail.com>
2021-10-24 17:31:55 -0400

    almost grpc-web

--- example/name_generator/ui/index.ts
+++ example/name_generator/ui/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, World!')
+// import { GetRandomNameRequest } from "unity/bazel-out/k8-fastbuild/bin/example/name_generator/v1/web_lib_pb/example/name_generator/v1/name_generator_pb";
+
+import { NameGeneratorServiceClient } from "unity/example/name_generator/v1/web_lib_pb/example/name_generator/v1/name_generator_service_grpc_web_pb";
+
+const nameGeneratorService = new NameGeneratorServiceClient('http://localhost:50051');
+
+// const request = new GetRandomNameRequest();
+// nameGeneratorService.getRandomName(request, {}, (err, response) => console.log(response));
