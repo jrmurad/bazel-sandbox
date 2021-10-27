67ab1b77185fe453421a025b1c900b0f98c2f73f
Jason Murad <jrmurad@gmail.com>
2021-10-27 00:04:45 -0400

    simplify

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ const path = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-  addons: ["@storybook/addon-essentials", "@storybook/addon-storyshots"],
   stories: ["../example/**/*.stories.@(js|jsx|ts|tsx)"],
   webpackFinal: async (config) => {
     config.resolve.plugins = [
