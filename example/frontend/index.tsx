6fda2b74d9073f9406ada3cc837cdbffdd292e72
ssilve1989 <steve@trumidtech.com>
2021-11-03 13:08:26 -0600

    refactor(esbuild): support esbuild bundles

--- example/frontend/index.tsx
+++ example/frontend/index.tsx
@@ -2,10 +2,11 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { ErrorBoundary } from "../common/frontend/ErrorBoundary";
 import { grpcWebImpl, RpcContext } from "../common/frontend/RpcContext";
+import NameGenerator from "../name_generator/frontend/NameGenerator";
 
-const NameGenerator = React.lazy(
-  () => import("unity/example/name_generator/frontend/NameGenerator")
-);
+// const NameGenerator = React.lazy(
+//   () => import("unity/example/name_generator/frontend/NameGenerator")
+// );
 
 ReactDOM.render(
   <div>
