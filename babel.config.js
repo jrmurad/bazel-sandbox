1575260ad026defc64b6032dba93b6e7ed62c6b8
Jason Murad <jrmurad@gmail.com>
2022-01-19 20:28:02 -0500

    fix build-storybook

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+  ],
   presets: [
     "@babel/preset-env",
     "@babel/preset-react",
