b4887fde1aeb5beb8d012949ef5ffc4e9afda53f
Jason Murad <jrmurad@gmail.com>
2022-01-21 18:01:11 -0500

    begin attempt with testcontainers

--- storyshots.puppeteer.test.ts
+++ storyshots.puppeteer.test.ts
@@ -3,10 +3,27 @@ import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
 import path from "path";
 import puppeteer from "puppeteer";
+import { GenericContainer } from "testcontainers";
+import yargs from "yargs/yargs";
+
+const CONTAINER_STORYBOOK_STATIC_DIR = "/opt/storybook-static";
+
+const argv = yargs(process.argv.slice(2))
+  .options({ storybook: { type: "string" } })
+  .parseSync();
+
+// determine directory based on whether running through bazel
+const hostStorybookStaticDir =
+  process.env.RUNFILES && argv.storybook
+    ? path.join(
+        process.env.RUNFILES,
+        process.env.BAZEL_WORKSPACE || "",
+        argv.storybook
+      )
+    : path.resolve(__dirname, "../storybook-static");
 
 initStoryshots({
   test: imageSnapshot({
-    // chromeExecutablePath: "/usr/bin/chromium-browser",
     customizePage: async (page) => {
       // disable animations and blinking cursors
       await page.addStyleTag({
@@ -32,8 +49,21 @@ initStoryshots({
       // avoid :hover
       await page.mouse.move(-1, -1);
     },
-    getCustomBrowser: () =>
-      puppeteer.connect({ browserWSEndpoint: "ws://localhost:3001" }),
+    getCustomBrowser: async () => {
+      const container = await new GenericContainer("browserless/chrome")
+        .withBindMount(
+          hostStorybookStaticDir,
+          CONTAINER_STORYBOOK_STATIC_DIR,
+          "ro"
+        )
+        .withEnv("ALLOW_FILE_PROTOCOL", "true")
+        .withExposedPorts(3000)
+        .start();
+
+      return puppeteer.connect({
+        browserWSEndpoint: `ws://localhost:${container.getMappedPort(3000)}`,
+      });
+    },
     getMatchOptions: ({ context }) => ({
       customSnapshotsDir: path.join(
         __dirname,
@@ -42,8 +72,6 @@ initStoryshots({
       ),
     }),
 
-    // mounted in a "browserless" docker container
-    // https://hub.docker.com/r/browserless/chrome
-    storybookUrl: "file:///opt/storybook-static",
+    storybookUrl: `file://${CONTAINER_STORYBOOK_STATIC_DIR}`,
   }),
 });
